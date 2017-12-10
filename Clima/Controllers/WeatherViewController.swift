c5021f7f1044e11f7fecd6ffd9b12f8ecffad43a
Jonathan Hernandez <jonathan@Jonathans-MacBook-Air.local>
2017-12-09 17:40:15 -0700

    request the user's location

--- Clima/Controllers/WeatherViewController.swift
+++ Clima/Controllers/WeatherViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewControllere: UIViewController {
-
+class WeatherViewControllere: UIViewController, CLLocationManagerDelegate {
+    
+    
+    private var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("latitude \(location.coordinate.latitude) longitude: \(location.coordinate.longitude)")
+
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 
 
