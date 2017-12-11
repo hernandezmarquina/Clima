c7d53a26a912ff1a8b6f71ca32c6d29c28ff1e01
Jonathan Hernandez <jonathan@Jonathans-MacBook-Air.local>
2017-12-10 23:59:13 -0700

    display weather by user locaiton

--- Clima/Controllers/WeatherViewController.swift
+++ Clima/Controllers/WeatherViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 import CoreLocation
+import SVProgressHUD
 
 class WeatherViewControllere: UIViewController, CLLocationManagerDelegate {
     
     
     private var locationManager = CLLocationManager()
+    private var weatherApi = WeatherAPI()
+    
+    @IBOutlet weak var weatherTempLabel: UILabel!
+    @IBOutlet weak var weatherIconImage: UIImageView!
+    @IBOutlet weak var weatherCityName: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +29,56 @@ class WeatherViewControllere: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let location = OWMLocation(lat:29.087184 , lon: -110.960144)
+        
+        getWeatherByUserLocation(owmLocation: location)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /**
+         Get weather by geographic coordinates.
+     
+     - parmeter owmLocation: Current user position in **OWMLocation** format
+     
+     */
+    private func getWeatherByUserLocation(owmLocation: OWMLocation){
+        
+        SVProgressHUD.show()
+        
+        weatherApi.getWeatherByOWMLocation(location: owmLocation, onSuccess: {(response) in
+            
+            SVProgressHUD.dismiss()
+            self.updateUIWithWeatherData(weatherData: response)
+            
+        }) {(error) in
+            
+            SVProgressHUD.dismiss()
+            self.updateUIWithError(error: error)
+            
+        }
+    }
+    
+    /**
+         Update UI with **Weather** data.
+     
+     - parameter weatherData: new weather data
+     
+     */
+    private func updateUIWithWeatherData(weatherData: Weather){
+        weatherCityName.text = weatherData.cityName
+        weatherIconImage.image = UIImage(named: weatherData.getWeatherIconName())
+        weatherTempLabel.text = "\(weatherData.temperature)º"
+    }
+    
+    private func updateUIWithError(error: String){
+        weatherCityName.text = error
+        weatherTempLabel.text = "0º"
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
